a74dbfb458a3054b8b3ab84f52371a01791746e6
Wright Gichana <wrightgichana@gmail.com>
2025-03-26 22:16:12 +0300

    fifth commit

--- Admin/src/Comp/Admin/Admin.jsx
+++ Admin/src/Comp/Admin/Admin.jsx
@@ -5,7 +5,7 @@ import AddProduct from '../AddProduct/AddProduct';
 import ListProduct from '../ListProduct/ListProduct';
 import { Routes,Route } from 'react-router-dom';
 import Orders from '../Orders/Orders';
-import Navbar from '../Navbar/Navbar';
+import Navbar from '../Navbar/navbar';
 const Admin = () => {
   return (
     <div className='admin'>
