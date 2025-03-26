56784b0bde6dc894123be0ba015d9a6f56924498
Wright Gichana <wrightgichana@gmail.com>
2025-03-26 21:09:52 +0300

    fifth commit

--- Admin/src/Comp/Admin/Admin.jsx
+++ Admin/src/Comp/Admin/Admin.jsx
@@ -4,11 +4,13 @@ import Sidebar from '../Sidebar/Sidebar';
 import AddProduct from '../AddProduct/AddProduct';
 import ListProduct from '../ListProduct/ListProduct';
 import { Routes,Route } from 'react-router-dom';
+import Orders from '../../Components/Orders/Orders';
 const Admin = () => {
   return (
     <div className='admin'>
       <Sidebar/>
       <Routes>
+      <Route path="orders" element={<Orders />} />
         <Route path='/addproduct' element={<AddProduct/>}/>
         <Route path='/listproduct' element={<ListProduct/>}/>
       </Routes>
