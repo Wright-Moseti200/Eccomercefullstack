fef3d1b0e66111301c3595b881fd689399fa5549
Wright Gichana <wrightgichana@gmail.com>
2025-03-26 23:01:47 +0300

    Initial commit

--- Admin/src/Component/Sidebar/Sidebar.jsx
+++ Admin/src/Component/Sidebar/Sidebar.jsx
@@ -4,14 +4,12 @@ import "./Sidebar.css";
 import {Link} from 'react-router-dom';
 import add_product_icon from '../../Assets/Product_Cart.svg';
 import list_product_icon from '../../Assets/Product_list_icon.svg';
-import orders_icon from '../../Assets/orders_icon.svg'; // You'll need to add this icon
 
 const Sidebar = () => {
   return (
     <div className='sidebar'>
       <Link to='/orders' style={{textDecoration:"none"}}>
         <div className='sidebar-item'>
-            <img src={orders_icon} alt="" className='sidebar-icon'/>
             <p>Orders</p>
         </div>
       </Link>
